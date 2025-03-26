b1970658758d39e167b0e577abb7459eacbc5783
poojathirumalai31 <pooja20051031@gmail.com>
2025-03-26 17:06:05 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function downloadResume() {
-    const resumeUrl = 'pooja_Resume.pdf';  // Change to actual filename if different
-    const a = document.createElement('a');
-    a.href = resumeUrl;
-    a.download = 'Pooja_Resume.pdf';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-}
